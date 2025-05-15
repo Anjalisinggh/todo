4ad3844cce08cfae9746c7f0ddc6171e0ac35d8d
anjalisinggh <anjalisingggh@gmail.com>
2025-05-15 21:22:29 +0530

    todo home page

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+// src/MyComponents/About.js
+
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1>About Page</h1>
+      <p>This is the About page of the Todo App.</p>
+    </div>
+  );
+};
+
+export default About;
